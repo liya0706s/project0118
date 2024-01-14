4540e63bc30347db165e61ea7f5e04ea95860b14
Angie <liya0706s@gmail.com>
2024-01-14 16:32:08 +0800

    add carousel with words

--- index.php
+++ index.php
@@ -367,6 +367,7 @@
 				<span class="visually-hidden">Next</span>
 			</button>
 		</div>
+		<!-- bootstrap carousel end -->
 
 		<!-- product plans start -->
 		<section id="product" class="bg-light mt-5">
@@ -419,10 +420,39 @@
 
 				</div>
 		</section>
-
 		<!-- product plans end -->
 
-		<!-- bootstrap carousel end -->
+		<!-- carousel with words start -->
+		<div class="container marketing">
+			<div class="row featurette">
+				<div class="col-md-7">
+					<h2 class="featurette-heading fw-normal lh-1">61688First featurette heading. <span class="text-body-secondary">It’ll blow your mind.</span></h2>
+					<p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+				</div>
+				<div class="col-md-5">
+					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+					</svg>
+				</div>
+			</div>
+
+			<hr class="featurette-divider">
+
+			<div class="row featurette">
+				<div class="col-md-7 order-md-2">
+					<h2 class="featurette-heading fw-normal lh-1">1234Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
+					<p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+				</div>
+				<div class="col-md-5 order-md-1">
+					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<!-- carousel with words end -->
 
 		<div class="container-fluid">
 			<div class="row">
