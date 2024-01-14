3b2b3e99bf3b36ae318eb8cdd9d49e8fe374a5d9
Angie <liya0706s@gmail.com>
2024-01-14 17:34:02 +0800

    建立reviews資料表

---
+++ front/reviews.php
@@ -0,0 +1,41 @@
+<?php
+include_once "../db.php"
+
+
+?>
+
+<div id="reviews" class="container marketing">
+			<div class="row featurette">
+				<div class="col-md-7">
+					<!-- title -->
+					<h2 class="featurette-heading fw-normal lh-1">55688First featurette heading.
+						<!-- subtitle -->
+						<span class="text-body-secondary">It’ll blow your mind.</span>
+					</h2>
+					<!-- review -->
+					<p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+				</div>
+				<div class="col-md-5">
+					<!-- image -->
+					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+					</svg>
+				</div>
+			</div>
+
+			<hr class="featurette-divider">
+
+			<div class="row featurette">
+				<div class="col-md-7 order-md-2">
+					<h2 class="featurette-heading fw-normal lh-1">4567Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
+					<p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+				</div>
+				<div class="col-md-5 order-md-1">
+					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+						<title>Placeholder</title>
+						<rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+					</svg>
+				</div>
+			</div>
+		</div>
--- index.php
+++ index.php
@@ -144,6 +144,10 @@
 								<a class="nav-link" aria-current="page" href="#product">Product</a>
 							</li>
 
+							<li class="nav-item">
+								<a class="nav-link" aria-current="page" href="#reviews">Reviews</a>
+							</li>
+
 							<!-- 動態生成的主選單和子選單 -->
 							<!-- 有子選單開始 -->
 							<!-- <li class="nav-item dropdown">
@@ -422,37 +426,11 @@
 		</section>
 		<!-- product plans end -->
 
-		<!-- carousel with words start -->
-		<div class="container marketing">
-			<div class="row featurette">
-				<div class="col-md-7">
-					<h2 class="featurette-heading fw-normal lh-1">61688First featurette heading. <span class="text-body-secondary">It’ll blow your mind.</span></h2>
-					<p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
-				</div>
-				<div class="col-md-5">
-					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-						<title>Placeholder</title>
-						<rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-					</svg>
-				</div>
-			</div>
-
-			<hr class="featurette-divider">
-
-			<div class="row featurette">
-				<div class="col-md-7 order-md-2">
-					<h2 class="featurette-heading fw-normal lh-1">1234Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
-					<p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-				</div>
-				<div class="col-md-5 order-md-1">
-					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-						<title>Placeholder</title>
-						<rect width="100%" height="100%" fill="var(--bs-secondary-bg)" /><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-					</svg>
-				</div>
-			</div>
-		</div>
-		<!-- carousel with words end -->
+		<!-- review area / carousel with words start -->
+		<?php
+			include "./front/reviews.php"
+		?>
+		<!-- review area / carousel with words end -->
 
 		<div class="container-fluid">
 			<div class="row">
