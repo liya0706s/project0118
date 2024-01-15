c601c2046095b2ef6ae2ce5cfc3731e072b75440
Angie <liya0706s@gmail.com>
2024-01-16 00:07:36 +0800

    新增back/review評論的後台，還要串api做修改確定的部分

--- back/reviews.php
+++ back/reviews.php
@@ -14,23 +14,43 @@
                     <th class="">刪除</th>
                 </tr>
                 <?php
-                $rows=$Reviews->all();
-                foreach($rows as $row){
+                $rows = $Reviews->all();
+                foreach ($rows as $row) {
                 ?>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-
-
+                    <tr>
+                        <td>
+                            <input type="text" name="title[]" value="<?= $row['title']; ?>">
+                            <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                            <!-- 隱藏的id才知道是哪一筆對應的title, subtitle和圖片 -->
+                        </td>
+                        <td><input type="text" name="subti[]" value="<?= $row['subti']; ?>"></td>
+                        <td><input type="text" name="review[]" value="<?= $row['review']; ?>"></td>
+                        <td>
+                            <img src="./img/<?= $row['img']; ?>">
+                        </td>
+                        <td>
+                            <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>>
+                        </td>
+                        <td>
+                            <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                        </td>
+                    </tr>
                 <?php
                 }
                 ?>
             </tbody>
         </table>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <input type="submit" value="修改確定">
+                        <input type="reset" value="重置">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+
     </form>
 </div>
