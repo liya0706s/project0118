c7314d134bb174a737876bd3b35b6d72ddd5f9ea
Angie <liya0706s@gmail.com>
2024-01-23 09:30:00 +0800

    carousel的rwd還沒有調整好

--- index.php
+++ index.php
@@ -320,28 +320,29 @@
 		<!-- alert 文字廣告結束 -->
 
 		<!-- bootstrap carousel start -->
-
-		<div id="carouselExampleAutoplaying" class="carousel slide mb-4" data-bs-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active" data-bs-interval="3000">
-					<img src="./img/slide-1.jpg" class="d-block w-100" alt="...">
-					<!-- <div style="background-image:url('./img/school.jpg');background-size:cover; "></div> -->
+		<div class="container-fluid">
+			<div id="carouselExampleAutoplaying" class="carousel slide mb-4" data-bs-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active" data-bs-interval="3000">
+						<img src="./img/slide-1.jpg" class="d-block w-100" alt="...">
+						<!-- <div style="background-image:url('./img/school.jpg');background-size:cover; "></div> -->
+					</div>
+					<div class="carousel-item" data-bs-interval="3000">
+						<img src="./img/slide-2.jpg" class="d-block w-100" alt="...">
+					</div>
+					<div class="carousel-item" data-bs-interval="3000">
+						<img src="./img/scratch-coding-1.jpg" class="d-block w-100" alt="...">
+					</div>
 				</div>
-				<div class="carousel-item" data-bs-interval="3000">
-					<img src="./img/slide-2.jpg" class="d-block w-100" alt="...">
-				</div>
-				<div class="carousel-item" data-bs-interval="3000">
-					<img src="./img/scratch-coding-1.jpg" class="d-block w-100" alt="...">
-				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>
 			</div>
-			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Next</span>
-			</button>
 		</div>
 		<!-- bootstrap carousel end -->
 
