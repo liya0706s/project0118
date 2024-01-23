78f5b74a6a2af3e36f79c039c910aa975d961c5b
Angie <liya0706s@gmail.com>
2024-01-23 08:38:59 +0800

    carousel height高度不限制

--- index.php
+++ index.php
@@ -323,14 +323,14 @@
 
 		<div id="carouselExampleAutoplaying" class="carousel slide mb-4" data-bs-ride="carousel">
 			<div class="carousel-inner">
-				<div class="carousel-item active" data-bs-interval="3000" style="height:864px">
+				<div class="carousel-item active" data-bs-interval="3000">
 					<img src="./img/slide-1.jpg" class="d-block w-100" alt="...">
 					<!-- <div style="background-image:url('./img/school.jpg');background-size:cover; "></div> -->
 				</div>
-				<div class="carousel-item" data-bs-interval="3000" style="height:864px">
+				<div class="carousel-item" data-bs-interval="3000">
 					<img src="./img/slide-2.jpg" class="d-block w-100" alt="...">
 				</div>
-				<div class="carousel-item" data-bs-interval="3000" style="height:864px">
+				<div class="carousel-item" data-bs-interval="3000">
 					<img src="./img/scratch-coding-1.jpg" class="d-block w-100" alt="...">
 				</div>
 			</div>
